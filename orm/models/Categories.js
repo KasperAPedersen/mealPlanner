f820b1ad6e072d2f45973f76cc3b283031310c99
Miebc09 <miebc@outlook.com>
2024-10-07 13:37:07 +0200

    Models added in orm

---
+++ orm/models/Categories.js
@@ -0,0 +1,16 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../database.js";
+
+let Categories = sequelize.define("Categories", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+export default Categories;

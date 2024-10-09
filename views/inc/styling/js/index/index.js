d533b4787e2585a5c33df0dc817f7a3d8da7d736
Miebc09 <miebc@outlook.com>
2024-10-09 10:08:56 +0200

    register modal

--- views/inc/styling/js/index/index.js
+++ views/inc/styling/js/index/index.js
@@ -33,3 +33,41 @@ let showLoginModal = () => {
         </form>
     `;
 }
+
+let showRegisterModal = () => {
+    document.getElementById('overlay').style.display = 'block';
+    let modal = document.getElementById('modal');
+    modal.style.display = 'block';
+    modal.className = "accountModal";
+    modal.innerHTML = `
+        <form action="/register" class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin" method="POST">
+            <h2 class="w3-center">Register</h2>
+            <i class="fa fa-times" onclick="closeModal();" aria-hidden="true"></i>
+            <div class="w3-row w3-section">
+              <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
+                <div class="w3-rest">
+                  <input class="w3-input w3-border" name="name" type="text" placeholder="Username">
+                </div>
+            </div>
+            <div class="w3-row w3-section">
+              <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-envelope"></i></div>
+                <div class="w3-rest">
+                  <input class="w3-input w3-border" name="email" type="email" placeholder="Email">
+                </div>
+            </div>
+            <div class="w3-row w3-section">
+              <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-key"></i></div>
+                <div class="w3-rest">
+                  <input class="w3-input w3-border" name="password" type="password" placeholder="Password">
+                </div>
+            </div>
+            <div class="w3-row w3-section">
+              <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-key"></i></div>
+                <div class="w3-rest">
+                  <input class="w3-input w3-border" name="confirmPassword" type="password" placeholder="Confirm Password">
+                </div>
+            </div>
+            <button class="w3-button w3-block w3-section w3-blue w3-ripple w3-padding" type="submit">Register</button>
+        </form>
+    `;
+}

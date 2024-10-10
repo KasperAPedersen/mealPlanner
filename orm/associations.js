658ba429c22e7b5c8bd73ed2034603a675e55392
Kasper Doe <kasper@doe.dk>
2024-10-10 09:39:33 +0200

    .
    .

--- orm/associations.js
+++
@@ -1,7 +0,0 @@
-// Anya
-
-import Ingredients from './models/Ingredients.js';
-import Categories from './models/Categories.js';
-
-Categories.hasMany(Ingredients, { foreignKey: 'category_id' });
-Ingredients.belongsTo(Categories, { foreignKey: 'category_id' });
--- orm/models.js
+++ orm/models.js
@@ -3,9 +3,6 @@
 // Import the Sequelize instance (configured database connection) from the database.js file
 import sequelize from './database.js';
 
-// Import the Sequelize class itself from the 'sequelize' package for defining models and datatypes
-import { Sequelize } from 'sequelize';
-
 // Import all the individual models representing different database tables/entities
 import Accounts from './models/Accounts.js';
 import Categories from './models/Categories.js';
@@ -15,12 +12,9 @@ import ShoppingLists from './models/ShoppingLists.js';
 import ShoppingListItems from './models/ShoppingListItems.js';
 import Units from './models/Units.js';
 
-//import './associations.js'; // Import and execute associations
-
 // Export all the models and the Sequelize instance as a single object
 // This allows easy import of all models and the Sequelize instance elsewhere in the application
 export default {
-    Sequelize, // Sequelize class, can be used to define new models or types
     sequelize, // Configured Sequelize instance (connected to the database)
     Accounts, // Accounts table model
     Categories, // Categories table model
